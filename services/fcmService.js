b7e3281fd1fcea509e83561953cb82a497f95b69
khanhlc04 <laothucatkhanhle@gmail.com>
2024-12-08 14:02:49 +0700

    Fix: Fix fcmService

--- services/fcmService.js
+++ services/fcmService.js
@@ -2,9 +2,24 @@ const https = require('https');
 const { google } = require('googleapis');
 const firebaseConfig = require('../config/firebaseConfig');
 
+const dotenv = require("dotenv");
+dotenv.config();
+
 const getAccessToken = () => {
     return new Promise(function (resolve, reject) {
-      const key = require('../placeholders/service-account.json');
+      const key = {
+        "type": process.env.type,
+        "project_id": process.env.project_id,
+        "private_key_id": process.env.private_key_id,
+        "private_key": process.env.private_key,
+        "client_email": process.env.client_email,
+        "client_id": process.env.client_id,
+        "auth_uri": process.env.auth_uri,
+        "token_uri": process.env.token_uri,
+        "auth_provider_x509_cert_url": process.env.auth_provider_x509_cert_url,
+        "client_x509_cert_url": process.env.client_x509_cert_url,
+        "universe_domain": process.env.universe_domain
+      }
       const jwtClient = new google.auth.JWT(
         key.client_email,
         null,
